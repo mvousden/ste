dac45381ee152de22dd08ba1b741b41e2fca9078
Mark Vousden <m.vousden@soton.ac.uk>
2021-05-24 11:45:28 +0100

    Support storing multiple previous characters.

--- template.c
+++ template.c
@@ -85,7 +85,7 @@ int template(FILE* inFile, FILE* outFile, char* dir)
 {
     /* Characters, read from inFile. */
     int thisChar;
-    int previousChar;
+    int previousChars[3];
 
     /* Moustache mode */
     int moustacheMode;  /* Are we in moustache mode? */
@@ -110,7 +110,9 @@ int template(FILE* inFile, FILE* outFile, char* dir)
     moustacheIndex = 0;
     moustacheMode = 0;
     error = 0;
-    previousChar = 0;
+    previousChars[0] = 0;
+    previousChars[1] = 0;
+    previousChars[2] = 0;
     thisChar = fgetc(inFile);  /* The first of many (hopefully). */
 
     /* Set up the moustache buffer. */
@@ -133,7 +135,7 @@ int template(FILE* inFile, FILE* outFile, char* dir)
         {
             /* Has the moustache ended? */
             if (thisChar == MOUSTACHE_CLOSE_CODE &&
-                previousChar == MOUSTACHE_CLOSE_CODE)
+                previousChars[2] == MOUSTACHE_CLOSE_CODE)
             {
                 /* If so, exit moustache mode, and remove the stray curly brace
                  * from the buffer. */
@@ -178,7 +180,7 @@ int template(FILE* inFile, FILE* outFile, char* dir)
         {
             /* Has a moustache started? */
             if (thisChar == MOUSTACHE_OPEN_CODE &&
-                previousChar == MOUSTACHE_OPEN_CODE)
+                previousChars[2] == MOUSTACHE_OPEN_CODE)
             {
                 /* Enter moustache mode. */
                 moustacheMode = 1;
@@ -217,7 +219,9 @@ int template(FILE* inFile, FILE* outFile, char* dir)
         }
 
         /* Next loop setup. */
-        previousChar = thisChar;
+        previousChars[0] = previousChars[1];
+        previousChars[1] = previousChars[2];
+        previousChars[2] = thisChar;
         thisChar = fgetc(inFile);
     }
 
